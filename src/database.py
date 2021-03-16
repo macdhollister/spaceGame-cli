407275008ab2723009000debb8286feccc724915
macdhollister <macdhollister@gmail.com>
2021-03-15 20:26:42 -0700

    Adding foreign key constraint enforcement

--- src/database.py
+++ src/database.py
@@ -1,17 +1,21 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker
 
 from dotenv import load_dotenv
 import os
 
+
+def _fk_pragma_on_connect(dbapi_con, con_record):
+    dbapi_con.execute('pragma foreign_keys=ON')
+
+
 load_dotenv()
 SQLALCHEMY_DATABASE_URL = os.environ["DATABASE_URL"] if "DATABASE_URL" in os.environ else "sqlite://"
-DEBUG = True if "DEBUG" in os.environ else False
-
-if DEBUG:
-    print("Database name: " + SQLALCHEMY_DATABASE_URL)
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
+
+event.listen(engine, 'connect', _fk_pragma_on_connect)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
