fb92a44ca7d837440ac8731d1366488a81edadaf
macdhollister <macdhollister@gmail.com>
2021-03-20 16:05:18 -0700

    Adding stealth level and detection level to Ship model

--- src/models/Ship.py
+++ src/models/Ship.py
@@ -13,6 +13,19 @@ def get_size(context):
         return len(context.get_current_parameters()['modules']) / 2
 
 
+def count_module(context, module):
+    modules = context.get_current_parameters()['modules']
+    return modules.count(module)
+
+
+def get_stealth_level(context):
+    return count_module(context, 'C')  # C represents an ecm suite
+
+
+def get_detection_level(context):
+    return count_module(context, 'S')  # S represents a sensor array
+
+
 class Ship(Base):
     __tablename__ = "ships"
 
@@ -20,8 +33,11 @@ class Ship(Base):
     modules = Column(String)
     owner = Column(String, ForeignKey(Faction.faction_name))
     location = Column(String, ForeignKey(Planet.name))
+
     max_hp = Column(Integer, default=get_size)
     hit_points = Column(Integer, default=get_size)
+    stealth_level = Column(Integer, default=get_stealth_level)
+    detection_level = Column(Integer, default=get_detection_level)
 
     owner_relationship = relationship(Faction, back_populates='ships')
     location_relationship = relationship(Planet, back_populates='ships')
