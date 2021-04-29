6bbef23b4ecc95b2e3b3e0847e22d92556a7fe73
macdhollister <macdhollister@gmail.com>
2021-04-29 00:12:54 -0700

    Adding more planetCrud tests

--- test/crud/test_planetCrud.py
+++ test/crud/test_planetCrud.py
@@ -1,35 +1,38 @@
 from src.crud import planetCrud
-from src.models import *
+from src.models import Planet
 
-map_fixture = [
-    {
-      "name": "planet_a",
-      "size": "s",
-      "resources": 4,
-      "connections": ["planet_b", "planet_c"]
-    },
-    {
-      "name": "planet_b",
-      "size": "m",
-      "resources": 3,
-      "connections": ["planet_a", "planet_d"]
-    },
-    {
-      "name": "planet_c",
-      "size": "l",
-      "resources": 2,
-      "connections": ["planet_a", "planet_d"]
-    },
-    {
-      "name": "planet_d",
-      "size": "m",
-      "resources": 4,
-      "connections": ["planet_b", "planet_c"]
-    }
-  ]
+from src.utils.planetUtils import SpecialPlanet
+from test.conftest import PlanetFactory
 
 
 def test_generate_planets(session):
+    map_fixture = [
+        {
+            "name": "planet_a",
+            "size": "s",
+            "resources": 4,
+            "connections": ["planet_b", "planet_c"]
+        },
+        {
+            "name": "planet_b",
+            "size": "m",
+            "resources": 3,
+            "connections": ["planet_a", "planet_d"]
+        },
+        {
+            "name": "planet_c",
+            "size": "l",
+            "resources": 2,
+            "connections": ["planet_a", "planet_d"]
+        },
+        {
+            "name": "planet_d",
+            "size": "m",
+            "resources": 4,
+            "connections": ["planet_b", "planet_c"]
+        }
+    ]
+
     planetCrud.build_map(session, map_fixture)
 
     assert session.query(Planet).count() == 4
@@ -48,3 +51,26 @@ def test_generate_planets(session):
           stored_planets
         )
       )
+
+
+def test_get_planets(session):
+    planet_a = PlanetFactory(name="planet_a", size="s", resources=4, special=SpecialPlanet.STANDARD)
+    planet_b = PlanetFactory(name="planet_b", size="m", resources=3, special=SpecialPlanet.LOGISTICS)
+    planet_a.connections = [planet_b]
+    planet_b.connections = [planet_a]
+
+    stored_planets = planetCrud.get_planets(session)
+    assert planet_a in stored_planets
+    assert planet_b in stored_planets
+    assert len(stored_planets) == 2
+
+
+def test_get_planet_names(session):
+    PlanetFactory(name="planet_a")
+    PlanetFactory(name="planet_b")
+
+    stored_names = planetCrud.get_planet_names(session)
+
+    assert "planet_a" in stored_names
+    assert "planet_b" in stored_names
+    assert len(stored_names) == 2
