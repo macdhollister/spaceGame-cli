bde1b323711049ebe0abb5112e2c5047ee3db825
macdhollister <macdhollister@gmail.com>
2021-04-29 00:12:29 -0700

    Adding Factories for testing DB models

--- test/conftest.py
+++ test/conftest.py
@@ -1,14 +1,36 @@
+import factory
 import pytest
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine, event
 
 from src import models
+from src.models import *
 
 Session = sessionmaker()
 
 
 def _fk_pragma_on_connect(dbapi_con, con_record):
     dbapi_con.execute('pragma foreign_keys=ON')
+
+
+class FacilityFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        model = Facility
+
+
+class FactionFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        model = Faction
+
+
+class PlanetFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        model = Planet
+
+
+class ShipFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        model = Ship
 
 
 @pytest.fixture(scope='module')
@@ -30,3 +52,11 @@ def session(connection):
     yield session
     session.close()
     transaction.rollback()
+
+
+@pytest.fixture(scope='function', autouse=True)
+def set_factory_session(session):
+    FacilityFactory._meta.sqlalchemy_session = session
+    FactionFactory._meta.sqlalchemy_session = session
+    PlanetFactory._meta.sqlalchemy_session = session
+    ShipFactory._meta.sqlalchemy_session = session
